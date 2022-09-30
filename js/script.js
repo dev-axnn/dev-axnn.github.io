6cdd2cdb40a490efc7a15255a0a536bdceb5ebf9
dev.axnn <dev.axnn@gmail.com>
2022-09-30 22:14:18 +0900

    스킬 섹션 치수 변경

--- js/script.js
+++ js/script.js
@@ -447,8 +447,8 @@ window.onload = function () {
 
   let bar_html = makeCircle(pro1, "#ff7200", "0.95");
   let bar_css = makeCircle(pro2, "#2196f3", "0.90");
-  let bar_js = makeCircle(pro3, "#ffd800", "0.80");
-  let bar_jq = makeCircle(pro4, "#aaa", "0.85");
+  let bar_js = makeCircle(pro3, "#ffd800", "0.75");
+  let bar_jq = makeCircle(pro4, "#aaa", "0.78");
   let bar_vue = makeCircle(pro5, "#20dd88", "0.60");
   let bar_scss = makeCircle(pro6, "#ff62b1", "0.70");
   let bar_figma = makeCircle(pro7, "#62f1ff", "0.85");
